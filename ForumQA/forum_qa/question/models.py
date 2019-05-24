a0098626173d7875f2b6282cc9c7275eb509f51a
Tomas-Tamantini <49096736+Tomas-Tamantini@users.noreply.github.com>
2019-05-24 13:32:11 -0300

    feat: Views para as perguntas (#4)

--- ForumQA/forum_qa/question/models.py
+++ ForumQA/forum_qa/question/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Question(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=100, verbose_name="Descrição breve")
+    content = models.TextField(verbose_name="Conteúdo da pergunta")
     # Date posted is automatically set to time when it is posted
     date_posted = models.DateTimeField(auto_now_add=True)
     # When user is deleted, question is also deleted
@@ -15,3 +16,6 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('question-detail', kwargs={'pk': self.pk})
