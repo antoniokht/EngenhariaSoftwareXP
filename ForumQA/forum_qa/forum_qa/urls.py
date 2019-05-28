6f0b8f0480dffde1cd28d1aa8466f423c8dbb87f
Tomas-Tamantini <49096736+Tomas-Tamantini@users.noreply.github.com>
2019-05-28 15:27:33 -0300

    feat: Todos os links funcionando. Excluir usuario (#8)
    * feat: Todos os links funcionando. Excluir usuario
    
    * feat: Respostas e perguntas na pagina de perfil

--- ForumQA/forum_qa/forum_qa/urls.py
+++ ForumQA/forum_qa/forum_qa/urls.py
@@ -28,7 +28,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('', include('question.urls')),
     path('answer/', include('answer.urls')),
+    path('user/', include('user.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
