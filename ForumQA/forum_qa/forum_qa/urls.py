082b34924daeceafda61f9811618d3a14677f8b3
Tomas-Tamantini <49096736+Tomas-Tamantini@users.noreply.github.com>
2019-05-28 13:03:16 -0300

    feat: Answer CRUD (#7)

--- ForumQA/forum_qa/forum_qa/urls.py
+++ ForumQA/forum_qa/forum_qa/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('', include('question.urls')),
+    path('answer/', include('answer.urls')),
 ]
 
 if settings.DEBUG:
